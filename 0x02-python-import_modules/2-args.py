4fe53491dd50273e381d116ca11e547608ba5001
Youssef Eddaif <jo9unreal@gmail.com>
2023-08-10 20:57:04 +0100

    Adding 2-args.py.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+from sys import argv
+
+if __name__ == "__main__":
+    num_args = len(argv)
+    if num_args < 2:
+        print("0 arguments.")
+    else:
+        print("{} arguments:".format(num_args))
+        # Skip the first arg (name of the file)
+    for index, arg in enumerate(argv[1:], 1):
+        print("{:d} : {}".format(index, arg))
