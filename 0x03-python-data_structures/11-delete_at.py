c9391a074bdbdb452102053e5d685cb5bd5a9ee4
Youssef Eddaif <jo9unreal@gmail.com>
2023-08-13 23:05:54 +0100

    Adjust the out of range condition.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def delete_at(my_list=[], idx=0):
-    if len(my_list) == 0 or len(my_list) < idx or idx < 0:
+    if len(my_list) == 0 or len(my_list) < idx + 1 or idx < 0:
         return my_list
     del my_list[idx]
     return my_list
