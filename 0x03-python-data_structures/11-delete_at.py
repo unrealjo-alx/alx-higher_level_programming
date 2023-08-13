c4960db5728ecd9d61b38aee0af8d1bf398cf60d
Youssef Eddaif <jo9unreal@gmail.com>
2023-08-13 23:03:08 +0100

    Using del statement instead of list comprehension.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -2,5 +2,5 @@
 def delete_at(my_list=[], idx=0):
     if len(my_list) == 0 or len(my_list) < idx or idx < 0:
         return my_list
-    my_list = [my_list[i] for i in range(len(my_list)) if i != idx]
+    del my_list[idx]
     return my_list
