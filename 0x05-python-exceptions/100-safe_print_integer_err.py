4d5c021dcac110fe1a0fc7a6ebb8dc769e35ff18
Youssef Eddaif <jo9unreal@gmail.com>
2023-08-30 23:20:18 +0100

    Adding a new line after the error is printed.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,6 +7,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except (ValueError, TypeError) as err:
-        sys.stderr.write(f"Exception: {err}")
+        sys.stderr.write(f"Exception: {err}\n")
         return False
     return True
