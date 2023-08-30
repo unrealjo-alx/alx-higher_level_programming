93df88950dbc2a76275dbe27078061c088af12ea
Youssef Eddaif <jo9unreal@gmail.com>
2023-08-30 23:17:30 +0100

    Safe integer print with error message.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_print_integer_err(value):
+    """A function that prints an integer."""
+    try:
+        print("{:d}".format(value))
+    except (ValueError, TypeError) as err:
+        sys.stderr.write(f"Exception: {err}")
+        return False
+    return True
