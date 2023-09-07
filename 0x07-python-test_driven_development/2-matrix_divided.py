2c783a79031daf5d1587e08b001f9d2c6ce25a8e
Youssef Eddaif <jo9unreal@gmail.com>
2023-09-07 20:59:44 +0100

    Fix matrix row size check.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -26,7 +26,7 @@ def matrix_divided(matrix, div):
     ):
         raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
 
-    if all(len(row) == matrix[0] for row in matrix) > 1:
+    if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
 
     if not isinstance(div, (int, float)):
