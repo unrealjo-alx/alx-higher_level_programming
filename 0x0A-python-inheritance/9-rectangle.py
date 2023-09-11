ad47f02f9e37594e9343fcfe75f2a6dcdfe44047
Youssef Eddaif <jo9unreal@gmail.com>
2023-09-12 00:41:56 +0100

    9. Full rectangle.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+9-rectangle.py - A module providing the Rectangle class.
+"""
+BaseGeometry = __import__("7-base_geometry").BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """
+    A class representing a rectangle.
+
+    Inherits from BaseGeometry.
+    """
+
+    def __init__(self, width, height):
+        """
+        Constructor for Rectangle class.
+
+        Args:
+            width (int): The width of the rectangle.
+            height (int): The height of the rectangle.
+
+        Raises:
+            TypeError: If width or height is not an integer.
+            ValueError: If width or height is less than or equal to 0.
+        """
+        self.__width = width
+        self.__height = height
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+
+    def area(self):
+        """
+        Calculates the area of the rectangle.
+
+        Returns:
+            int: The area of the rectangle.
+        """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """Returns a string representation of the rectangle."""
+        return f"[Rectangle] {self.__width}/{self.__height}"
+
+    def __repr__(self):
+        """Returns a string representation of the rectangle object."""
+        return f"Rectangle({self.__width}, {self.__height})"
