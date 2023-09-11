e3aae356cdbdd15160d5adc4a2c8b2661e3e066b
Youssef Eddaif <jo9unreal@gmail.com>
2023-09-12 00:16:37 +0100

    5. Geometry module.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+5-base_geometry.py - A module providing the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry-related classes.
+    """
+
+    pass
