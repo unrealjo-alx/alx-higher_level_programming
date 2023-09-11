027038e9d8973cf1f54704196f7df98f506f86b1
Youssef Eddaif <jo9unreal@gmail.com>
2023-09-12 00:21:28 +0100

    6. Improve Geometry.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+6-base_geometry.py - A module providing the BaseGeometry class.
+"""
+
+
+class BaseGeometry:
+    """
+    Base class for geometry-related classes.
+    """
+
+    def area(self):
+        """
+        Calculates the area.
+        """
+        raise Exception("area() is not implemented")
