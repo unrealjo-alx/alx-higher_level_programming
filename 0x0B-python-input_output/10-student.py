cda8c6ef48d79800b6c5568b74b9575c8b4ab248
Youssef Eddaif <jo9unreal@gmail.com>
2023-09-12 19:22:22 +0100

    10. Student to JSON with filter.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+10-student: A module providing the Student class.
+"""
+
+
+class Student:
+    """A class Student that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """The constructor
+
+        Args:
+            first_name (str): The first name.
+            last_name (str): The last name.
+            age (int): The age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance
+
+        Args:
+            attrs (list, optional): A list of strings,. Defaults to None.
+
+        Returns:
+            dict:  a dictionary representation.
+        """
+        if not isinstance(attrs, list):
+            return self.__dict__
+
+        res = {}
+        for attr in attrs:
+            if self.__dict__.get(attr) is not None:
+                res[attr] = self.__dict__.get(attr)
+        return res
