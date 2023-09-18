f61ade1e77ef3993ca8ed6de6e6e10451b8867a3
Youssef Eddaif <jo9unreal@gmail.com>
2023-09-18 23:44:57 +0100

    11. Square size.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -12,6 +12,17 @@ class Square(Rectangle):
         """Initialize a Square object."""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        """Get the size of the square."""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """Set the size of the square."""
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """Return a string representation of the Square instance."""
         coordinates = f"{self._x}/{self._y}"
