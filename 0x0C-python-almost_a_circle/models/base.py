cd4f97685a3bcab5b712981f638152fe47e47112
Youssef Eddaif <jo9unreal@gmail.com>
2023-09-18 23:57:11 +0100

    Refactor the save_to_file method.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,5 +45,6 @@ class Base:
             list_objs = []
         filename = cls.__name__ + ".json"
         with open(filename, "w", encoding="utf-8") as file:
-            json_string = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
+            dicts_list = [obj.to_dictionary() for obj in list_objs]
+            json_string = cls.to_json_string(dicts_list)
             file.write(json_string)
