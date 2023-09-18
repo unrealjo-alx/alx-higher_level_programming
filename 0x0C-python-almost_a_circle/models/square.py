166c2148cb854dbc5843b9b2caf2fc5ad8545ad0
Youssef Eddaif <jo9unreal@gmail.com>
2023-09-18 23:46:06 +0100

    12. Square update.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -27,3 +27,24 @@ class Square(Rectangle):
         """Return a string representation of the Square instance."""
         coordinates = f"{self._x}/{self._y}"
         return f"[Square] ({self.id}) {coordinates} - {self.width}"
+
+    def update(self, *args, **kwargs):
+        """Assign attributes based on arguments and keyword arguments."""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self.x = args[2]
+            if len(args) >= 4:
+                self.y = args[3]
+        else:
+            if "id" in kwargs:
+                self.id = kwargs["id"]
+            if "size" in kwargs:
+                self.size = kwargs["size"]
+            if "x" in kwargs:
+                self.x = kwargs["x"]
+            if "y" in kwargs:
+                self.y = kwargs["y"]
