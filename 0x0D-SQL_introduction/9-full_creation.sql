7b4dca00ba8224e05963f208ac42d20db916949a
Youssef Eddaif <jo9unreal@gmail.com>
2023-10-19 22:48:46 +0100

    Fix: the script fails if a table already exists.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,6 +1,11 @@
 -- Creates a table second_table and  add multiples rows.
 
-CREATE TABLE second_table( id INT, name VARCHAR(256), score INT) ;
+CREATE TABLE
+    IF NOT EXISTS second_table(
+        id INT,
+        name VARCHAR(256),
+        score INT
+    );
 
 INSERT INTO
     second_table (id, name, score)
