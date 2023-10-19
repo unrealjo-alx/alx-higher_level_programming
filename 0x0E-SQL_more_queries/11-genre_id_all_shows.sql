52661938599fad707840edc4a923fc3676930f6e
Youssef Eddaif <jo9unreal@gmail.com>
2023-10-19 23:49:50 +0100

    11. Genre ID for all shows.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -0,0 +1,10 @@
+-- List shows: title, genre ID. NULL if no genre. Sort by title and genre ID.
+
+SELECT
+    tv_shows.title,
+    tv_show_genres.genre_id
+FROM tv_shows
+    LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+ORDER BY
+    tv_shows.title ASC,
+    tv_show_genres.genre_id ASC;
