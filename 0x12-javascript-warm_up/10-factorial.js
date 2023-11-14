f4c69180f0a17010dd1e52b0259718ca8f28494e
Youssef Eddaif <jo9unreal@gmail.com>
2023-11-14 13:00:10 +0100

    Apply Rules of Standard.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -2,7 +2,7 @@
 const args = process.argv;
 const num = parseInt(args[2] ?? 1);
 function fact (n) {
-  if (n == 1) return 1;
+  if (n === 1) return 1;
   return n * fact(n - 1);
 }
 console.log(fact(num));
