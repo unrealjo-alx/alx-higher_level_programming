1942cf0e1e6b6ec90e55e54835e11c1f7fc294ad
Youssef Eddaif <jo9unreal@gmail.com>
2023-12-16 21:31:47 +0100

    2. Filter states by user input.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script that displays all values in the states table of hbtn_0e_0_usa
+where name matches the argument.
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    """
+    Main entry point of the script.
+    """
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+    )
+
+    cursor = db.cursor()
+    query = "SELECT * FROM states WHERE name = '{}' ORDER BY states.id ASC"
+    cursor.execute(query.format(state_name))
+    result = cursor.fetchall()
+
+    for row in result:
+        print(row)
+
+    cursor.close()
+    db.close()
