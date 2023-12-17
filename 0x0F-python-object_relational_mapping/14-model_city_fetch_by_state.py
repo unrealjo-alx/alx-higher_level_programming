26574a43e895efae3308ed4b6821f5137dd64194
Youssef Eddaif <jo9unreal@gmail.com>
2023-12-17 22:16:49 +0100

    14. Cities in state.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Script fetches and prints all City objects from the database hbtn_0e_14_usa
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+from model_city import City
+
+
+if __name__ == "__main__":
+    """
+    Main entry point of the script.
+    """
+    if len(sys.argv) != 4:
+        exit(1)
+
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}:{password}@localhost/{database}",
+        pool_pre_ping=True,
+    )
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    cities = session.query(City).join(State).order_by(City.id).all()
+
+    for city in cities:
+        print(f"{city.state.name}: ({city.id}) {city.name}")
+
+    session.close()
