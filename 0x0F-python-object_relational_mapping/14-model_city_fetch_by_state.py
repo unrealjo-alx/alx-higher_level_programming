a9f549d6cb77913e4fa1ae3b888a9c5fa570967a
Youssef Eddaif <jo9unreal@gmail.com>
2023-12-17 22:24:02 +0100

    Improve the solution.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -4,7 +4,7 @@ Script fetches and prints all City objects from the database hbtn_0e_14_usa
 """
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 from model_city import City
 
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     Main entry point of the script.
     """
     if len(sys.argv) != 4:
-        exit(1)
+        sys.exit(1)
 
     username = sys.argv[1]
     password = sys.argv[2]
@@ -25,12 +25,15 @@ if __name__ == "__main__":
         pool_pre_ping=True,
     )
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(engine)
 
-    cities = session.query(City).join(State).order_by(City.id).all()
+    results = (
+        session.query(State.name, City.id, City.name)
+        .join(City, City.state_id == State.id)
+        .order_by(City.id)
+    )
 
-    for city in cities:
-        print(f"{city.state.name}: ({city.id}) {city.name}")
+    for state_name, city_id, city_name in results:
+        print(f"{state_name}: ({city_id}) {city_name}")
 
     session.close()
