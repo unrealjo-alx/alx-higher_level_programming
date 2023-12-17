9769149c2bf6c5ecf37c3d91a13a013be559190c
Youssef Eddaif <jo9unreal@gmail.com>
2023-12-17 21:42:53 +0100

    Refactor the code #9.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,17 +1,21 @@
 #!/usr/bin/python3
 """
-    Script lists all State objects that contain the letter 'a'
-	from the database hbtn_0e_6_usa
+Script lists all State objects that contain the letter 'a'
+from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
+
 
 if __name__ == "__main__":
     """
     Main entry point of the script.
     """
+    if len(sys.argv) != 4:
+        exit(0)
+
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
@@ -21,11 +25,11 @@ if __name__ == "__main__":
         pool_pre_ping=True,
     )
     Base.metadata.create_all(engine)
-
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    session = Session(engine)
 
     result = session.query(State).filter(State.name.like("%a%")).order_by(State.id)
+
     for state in result:
         print("{}: {}".format(state.id, state.name))
+
     session.close()
