99018568cee991f9c0650b7dd2ea5afa41a883ad
Youssef Eddaif <jo9unreal@gmail.com>
2023-12-17 22:06:45 +0100

    Create City model.
    Signed-off-by: Youssef Eddaif <jo9unreal@gmail.com>

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+model_city.py : a file that contains the class definition of a City
+"""
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class City(Base):
+    """City Class"""
+
+    __tablename__ = "cities"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
